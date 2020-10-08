0a8debb83ce8da28a6a7ff58b3205b2dd7f74e1e
César Nascimento <oi@ncesar.com>
2020-10-08 17:30:17 -0300

    single file redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case 'DECREMENT_COUNT':
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+const INITIAL_STATE = {
+  count: 0,
+};
+
+const store = createStore(reducer, INITIAL_STATE);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>hi</div>
+      <Counter />
+    </Provider>
+  );
+}
+
+function Counter() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  function incrementCount() {
+    dispatch({
+      type: 'INCREMENT_COUNT',
+    });
+  }
+  function decrementCount() {
+    dispatch({
+      type: 'DECREMENT_COUNT',
+    });
+  }
+
+  return (
+    <>
+      <h2>counter: {count}</h2>
+      <button onClick={incrementCount}>increment</button>
+      <button onClick={decrementCount}>decrement</button>
+    </>
   );
 }
 
